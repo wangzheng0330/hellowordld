d776f2062ed517e8df5fd9c7e895f74625a1781d
wangzheng <6937671@qq.com>
2018-12-17 20:56:04 +0800

    First Commit

---
+++ helloword/helloword/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  helloword
+//
+//  Created by 王铮 on 2018/12/17.
+//  Copyright © 2018年 王铮. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "测试一下我的第一个github项目(捂脸)"<<endl;
+    return 0;
+}
